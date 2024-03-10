08460f3e227d5e0485dd6dda718bfb4750ce0a3a
trustme6 <trustmeeverydayandnow@gmail.com>
2024-03-10 16:20:35 +0400

    update stories page

--- src/components/storyPostItem/storyPostItem.vue
+++ src/components/storyPostItem/storyPostItem.vue
@@ -1,31 +1,54 @@
 <template>
-    <div class="c-story-post-item">
-        <div class="stories-container">
-            <div class="header">
-                <div class="progress">
-                </div>
-                <div class="user"></div>
-            </div>
-            <div class="content"></div>
-            <div class="button"></div>
-        </div>
-        <template></template>
-    </div>
+    <div class="wrapper-stories">
+<button class="prev">
+<span class="icon">
+<icon name="arrow" />
+</span>
+</button>
+<div class="c-story-post-item">
+<div class="stories-container">
+<div class="header">
+<div class="progress">
+<xProgress />
+</div>
+<div class="user"></div>
+</div>
+<div class="content">
+ <div class="loader" v-if="loading">
+                loading...
+</div>
+<div class="info" v-else>
+<div v-if="data.content?.length" class="content-text" v-html="data.content"></div>
+<div v-else>some text</div>
+</div>
+</div>
+<div class="button"><xButton /></div>
+</div>
+</div>
+<button class="next">
+<span class="icon">
+<icon name="arrow" />
+</span>
+</button>
+</div>
 </template>
 
 <script>
+/* eslint-disable */
 import { xButton } from '../button'
 import { xProgress } from '../progress'
-import { placeholder } from '../placeholder'
-import { spinner } from '../spinner'
+import { icon } from '../../icons'
+// import { placeholder } from '../placeholder'
+// import { spinner } from '../spinner'
 
 export default {
     name: "StoryUserItem",
     components: {
         xButton,
         xProgress,
-        placeholder,
-        spinner
+        icon
+        // placeholder,
+        // spinner
     },
     props: {
         active: Boolean,
@@ -39,4 +62,4 @@ export default {
 }
 </script>
 
-<style lang="scss" src="./StoryPostItem.scss"></style>
+<style lang="scss" src="./storyPostItem.scss"></style>
--- src/pages/stories/stories.vue
+++ src/pages/stories/stories.vue
@@ -1,11 +1,17 @@
 <template>
-<div>
+<div class="wrapper">
 <h1>Stories</h1>
+<storyPostItem />
 <router-view />
 </div>
 </template>
 <script>
+import storyPostItem from '@/components/storyPostItem/storyPostItem.vue'
+
 export default {
-  name: 'Stories'
+  name: 'Stories',
+  components: {
+    storyPostItem
+  }
 }
 </script>
